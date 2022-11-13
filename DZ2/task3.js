21f3f2570ea9d7cfe8c8caa1b10defed17bc67f6
KaterinaSergeeva <kater.sergeeva98.com>
2022-11-13 21:20:14 +0300

    complete dz2 task3

---
+++ DZ2/task3.js
@@ -0,0 +1,15 @@
+export function circleInSquare() {
+    const circle = 8;
+    const square = 16;
+    if ((circle/square)<=(Math.PI/4)){ 
+        console.log("Круг поместится в квадрат");
+    } else {
+        console.log("Круг поместится в квадрат");
+    }
+}
+
+
+
+/* *В переменных circle и square хранятся площади круга
+и квадрата соответственно. Написать программу,
+которая определяет, поместится ли круг в квадрат. */
