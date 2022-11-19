087c7d4d04cde02966011047d21e2895e632194c
KaterinaSergeeva <kater.sergeeva98.com>
2022-11-19 20:23:58 +0300

    complete DZ5

---
+++ DZ5/task.js
@@ -0,0 +1,21 @@
+export function massives(){
+   let firstArr = Array.from({length: 10}, () => Math.floor(Math.random() * 100));
+   let secondArr = firstArr.map((num) => num * 2);
+   let maxOfFirstArr = Math.max.apply(null,firstArr);
+   let minfFirstArr = Math.min.apply(null,firstArr);
+   console.log(firstArr);
+   console.log(secondArr);
+   console.log(maxOfFirstArr);
+   console.log(minfFirstArr);
+}
+
+/* Создайте массив целых чисел из 10 элементов.
+
+1.Выведите в консоль сумму всех элементов массива.
+2.Создайте новый массив на основе исходного, в
+котором каждый элемент будет вдвое больше
+элемента исходного массива с таким же индексом.
+(a[1] = 3, b[1] = 6, где a — исходный массив, b — новый
+массив).
+3.*Найдите и выведите в консоль наибольший и
+наименьший элементы исходного массива. */
